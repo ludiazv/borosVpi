9cea4527bd78792bd56fa8d76937ebf508100b78
L Diaz <ldiaz@navarone.local>
2019-10-25 10:16:27 +0200

    fix

--- firmware_borosVPi/tools.py
+++ firmware_borosVPi/tools.py
@@ -75,7 +75,7 @@ def install(dos):
     try:
         m = json.loads(manr.read().decode(manr.info().get_param('charset') or 'utf-8'))
     except:
-        m = json.loads(manr.read().decode('utf-8'))
+        m = json.loads(manr.read())
 
     manr.close()
     
@@ -94,7 +94,7 @@ def install(dos):
     try:
         m = json.loads(manr.read().decode(manr.info().get_param('charset') or 'utf-8'))
     except:
-        m = json.loads(manr.read().decode('utf-8'))
+        m = json.loads(manr.read())
     manr.close()
 
     fdos=""
