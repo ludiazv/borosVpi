d209711c368c1092cfa91a150c0fa4689b2daaec
L Diaz <ldiaz@navarone.local>
2019-10-25 11:53:19 +0200

    fix bulid

--- vpid/buildall.sh
+++ vpid/buildall.sh
@@ -3,11 +3,31 @@
 #ARCHS=( "armv7-unknown-linux-gnueabihf" "armv7-unknown-linux-musleabihf" "aarch64-unknown-linux-musl" "aarch64-unknown-linux-gnu" )
 ARCHS=( "arm-unknown-linux-musleabihf" "armv7-unknown-linux-musleabihf" "aarch64-unknown-linux-musl")
 
-echo "Vpi cross compilation script"
+echo "Vpi cross compilation script..."
+echo "==============================="
+printf "Check cross tools..."
+cargo install --list | grep cross
+if [ $? -eq 0 ] ; then
+    echo "not installed. force install"  
+    cargo install cross --force
+else
+    echo "Installed!"
+fi
+prinf "Check cargo-deb tool..."
+cargo install --list | grep cargo-deb
+if [ $? -eq 0 ] ; then
+    echo "not installed. force install"
+    cargo install cargo-deb --force
+else
+    echo "Installed!"
+fi
 
 for i in "${ARCHS[@]}"
 do
     echo "Building for Release $i"
     cross build --target=$i --release
 done
+
+echo "==============================="
+exit 0
 
