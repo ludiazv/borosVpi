51e6ed349621e5de254cf4e75f0acebfa057cafb
L Diaz <ldiaz@navarone.local>
2020-09-23 19:03:00 +0200

    testing version of systemd service

--- vpid/scripts/service.sh
+++ vpid/scripts/service.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
-
-
 if [ "$1" == "clean" ] ; then
 
     echo "Cleaning service..."
     sudo systemctl stop vpid.service
     sudo rm /lib/systemd/system/vpid.service
+    sudo rm /etc/vpid/vpidEnv
+    sudo rm /etc/vpid/vpid.yml
     sudo systemctl daemon-reload
-    rm -R /etc/vpid
+    #sudo rm -Rf /etc/vpid
     exit 0
 
 fi
 
 echo "Install service for testig..."
-mkdir -p /etc/vpid
-cp vpiEnv       /etc/vpid/vpidEnv
-cp vpid.yml     /etc/vpid/vpid.yml
-cp vpid.service /lib/systemd/system/vpid.service
+sudo mkdir -p /etc/vpid
+sudo cp vpidEnv       /etc/vpid/vpidEnv
+sudo cp vpid.yml     /etc/vpid/vpid.yml
+sudo cp vpid.service /lib/systemd/system/vpid.service
+sudo systemctl daemon-reload
+sudo systemctl start vpid.service
+sudo journalctl -u vpid.service -f
 
 
