b7bc0e569d5acca873e34e4a0ba2d0a2d12764de
L Diaz <ldiaz@navarone.local>
2020-09-23 15:54:59 +0200

    Add new snafu errors(Json,Send,receive) + from error of lua errors

--- vpid/vpid/src/error.rs
+++ vpid/vpid/src/error.rs
@@ -1,6 +1,8 @@
-pub use snafu::{Snafu, ResultExt};
+pub use snafu::{Snafu, ResultExt,OptionExt};
 //, Backtrace, ErrorCompat, ensure};
 use std::{path::{PathBuf}};
+use crate::cmd::VpiCommand;
+use crossbeam_channel::{RecvTimeoutError,SendTimeoutError};
 
 #[derive(Debug, Snafu)]
 #[snafu(visibility(pub(crate)))]
@@ -15,7 +17,21 @@ pub enum Error {
     SockBind { sock: PathBuf, source: std::io::Error },
     #[snafu(display("Could not configure vpi board: {}", source ))]
     VpiConfigureError { source: vpi::Error },
+    #[snafu(display("Command parse failed, Unkwnon command vpi: {}", cmd ))]
+    CommandParse { cmd: String },
+    #[snafu(display("Command could not be sent: {}", source ))]
+    CommandSend { source: SendTimeoutError<VpiCommand> },
+    #[snafu(display("Command response failed or timedout: {}", source ))]
+    CommandRecv { source: RecvTimeoutError },
+    #[snafu(display("Invalid JSON: {}", source ))]
+    JsonError { source: serde_json::error::Error },
 
 }
 
 pub type Result<T, E = Error> = std::result::Result<T, E>;
+
+impl std::convert::From<Error> for rlua::Error {
+    fn from(s: Error) -> Self {
+        rlua::Error::RuntimeError(format!("vpi-lua error:{}",s))
+    }
+}   
