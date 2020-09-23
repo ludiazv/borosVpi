ec825cfbb9640c53a5f0284f96703786f116b998
L Diaz <ldiaz@navarone.local>
2020-09-23 19:07:08 +0200

    tester board support

--- firmware_borosVPi/misc/main.py
+++ firmware_borosVPi/misc/main.py
@@ -2,6 +2,7 @@ import machine
 import time
 from machine import Pin
 from machine import I2C
+from machine import Timer
 import network
 import ssd1306
 from ina219 import INA219
@@ -25,6 +26,11 @@ aBut=Pin(14,Pin.OUT,value=1)
 
 # I2c inerface
 I2c=I2C(scl=Pin(5), sda=Pin(4), freq=100000)
+# Timer for pseudo pwm for current & counter
+Tim=Timer(-1)
+Tmc=0
+Port_last=0x0F
+Pseudo_pwm = [0,0,0,0,0,0,0,0,0,0]
 # Oled screen
 Oled=None
 #IO expander
@@ -44,6 +50,25 @@ print("Web server opened")
 print(Ap.ifconfig())
 print("Type run() to run the tests")
 
+
+def set_current(n):
+    global Port_last
+    if n != Port_last:
+        Io.port = (Io.port & 0xF0) |  (0x0F & n)
+        Port_last = n
+        #print("Io Status {} {:02X}h".format(n,Io.port))
+
+def current_callback(a):
+    global Tmc
+    set_current(Pseudo_pwm[Tmc])
+    Tmc= 0 if Tmc >= 9 else Tmc + 1
+
+
+def start_timer():
+    Tim.init(period=100,mode=Timer.PERIODIC,callback=current_callback)
+def stop_timer():
+    Tim.deinit()
+
 def col(n):
     return "\033[32m{}\033[0m".format(n) if n else "\033[31m{}\033[0m".format(n)
 
@@ -52,6 +77,7 @@ def init_i2c():
     global Ina
     global Io
     global Firmware_found
+    global Tim
     #reset_board()
     #boot_loader=I2c.is_ready(0x22)
     devs=I2c.scan()
@@ -60,16 +86,20 @@ def init_i2c():
         Oled=ssd1306.SSD1306_I2C(128, 64, I2c)
         Oled.poweron()
         Oled.fill(0)
-        #Oled.contrast(255)
+        Oled.contrast(255)
+        inf("Init!",1000)
         Oled.show()
     if 0x40 in devs:
         print("INA219 detected")
-        Ina=INA219(0.1, I2c)
-        Ina.configure()
+        Ina=INA219(0.1, I2c,max_expected_amps=2.5)
+        Ina.configure(voltage_range=INA219.RANGE_16V)
     if 0x20 in devs:
         print("PCF8574 io expander detected ",end='')
         Io=PCF8574(I2c)
+        time.sleep_ms(500)
+        print("IO:{:02X}h".format(Io.port) )
         set_current(0)
+        Tmc=0
     if 0x22 in devs:
         print("Bootloader found")
     if 0x33 in devs:
@@ -79,15 +109,22 @@ def init_i2c():
     return (Ina is not None) and (Io is not None)
 
 
-def set_current(n):
-    Io.port = (Io.port & 0xF0) |  (0x0F & n)
-    print("Io Status {:02X}h".format(Io.port))
+
         
 
 def inf(s,delay=0):
     if Oled is not None:
-        Oled.fill(0)
-        Oled.text(s,0,10)
+        s2=s.replace('\033[0m','').replace('\033[32m','').replace('\033[31m','')
+        s2= s2[:112] if len(s2) > 112 else s2
+        lines = len(s2) // 16
+        lines = lines+1 if (len(s2) % 16)!= 0 else lines
+        #Oled.fill(0)
+        Oled.scroll(0, lines* 8)
+        Oled.fill_rect(0,0,127,8*lines,0) 
+        for l in range(lines):
+            Oled.text(s2[l*16:200],0,8*(l+1))
+
+        #Oled.text(s2,0,10)
         Oled.show()
     print(s)
     time.sleep_ms(delay)
@@ -108,18 +145,22 @@ def click(but,ms,post=0):
 
 
 def test_is_off():
-    v=Ina.voltage()
-    return v<0.1
+    v=Ina.supply_voltage()
+    return v<1.0
 
 def test_is_on():
-    v=Ina.voltage()
+    v=Ina.supply_voltage()
     return v>4.50
 
-def current_monitor(n=0):
+def current_monitor(n=1,or_condition=None):
     while n>0:
-        inf("{:.2f} V {:.2f} mA".format(Ina.voltage(),Ina.current() ))
+        inf("{:.2f}V {:.1f}mA".format(Ina.supply_voltage(),Ina.current() ))
+        if or_condition is not None:
+            if or_condition():
+                return True
         time.sleep(1)
         n=n-1
+    return False
     
 def upload_firmware():
     inf("Up...",500)
@@ -128,7 +169,7 @@ def upload_firmware():
  
 def run():
     global Pi
-    init_i2c()
+    #init_i2c()
     if not Firmware_found:
         inf("VPiFW not found",50)
         upload_firmware()
@@ -144,17 +185,58 @@ def run():
     inf("ID {}".format(Pi.id()),200)
     inf(Pi.dump_conf())
     set_current(3) # Perform all basic test with 1A load
-    current_monitor(1)
+    time.sleep_ms(500)
+    current_monitor(2)
     # Each function is a test set
-    #test_on_off()
-    #but_and_irq()
-    #beep()
+    test_on_off()
+    but_and_irq()
     wdg_wake()
-    
+    beep()
+    #fan()
+    test_dynamic_current()
+    test_full_current()
+
     Led.value(1)
     Pi.cmd(vpi.CMD_HARD)
+    set_current(0)
     
+def test_dynamic_current():
+    global Pseudo_pwm
+    inf("# Section - dyn current")
+    # Reset Pseudo pwm
+    for i in range(10):
+        Pseudo_pwm[i]=0
     
+    # Iterate all currents
+    for i in range(41):
+        # Full part
+        l=i//4
+        for j in range(l):
+            Pseudo_pwm[j]=0x0F
+
+        if l<10:
+            r= i % 4
+            v= 3 if r==2 else r
+            v= 7 if r==3 else v
+            Pseudo_pwm[l]=v
+        
+        start_timer()
+        volt=10
+        current=0
+        for j in range(10):
+            s= Ina.supply_voltage()
+            volt = s if s<volt else volt
+            current += Ina.current()
+            time.sleep_ms(100)
+        stop_timer()
+        inf(">{}/{:.2f}V {:.1f}mA".format(i,volt,current/10))
+    set_current(0)
+
+def test_full_current():
+    inf("# Section Test full current")
+    set_current(0x0f)
+    current_monitor(5*60)
+    set_current(0)        
     
 def test_on_off():
      # Section 1 - Basic on-off
@@ -180,7 +262,7 @@ def test_on_off():
     Pi.cmd(vpi.CMD_SHUT)
     Pi.boot()
     Pi.cmd(vpi.CMD_SHUT)
-    current_monitor(16) # Grace time is 15
+    current_monitor(16,test_is_off) # Grace time is 15
     inf("==>Is off:{} Vbus:{}".format(col(Pi.is_running() == False), col(test_is_off())))
     
 def but_and_irq():
@@ -230,7 +312,7 @@ def wdg_wake():
     Pi.wake(1)
     current_monitor(2)
     Pi.cmd(vpi.CMD_HARD)
-    current_monitor(61)
+    current_monitor(121,test_is_on)
     inf("==>Wake:{}".format( col(test_is_on()) ))
     Pi.boot()
 
@@ -244,6 +326,8 @@ def beep():
     inf("==>Beep:Check sound")
 
 
+# Init I2C
+init_i2c()
 
 
     
--- firmware_borosVPi/misc/test.sh
+++ firmware_borosVPi/misc/test.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-echo "Running tester session"
-networksetup -setairportnetwork en2 VpiTester
-sleep 5
-telnet -K 192.168.4.1
+WIFI_INTERFACE="en2"
+echo "Running tester automation"
+echo "========================="
+echo "¡script only works in macOs!"
+echo "Enable wifi please"
+networksetup -getairportpower $WIFI_INTERFACE
+networksetup -setairportpower $WIFI_INTERFACE on
+networksetup -setairportnetwork $WIFI_INTERFACE VpiTester
+sleep 6
+{ echo; echo 'run()'; echo; echo 'quit()'; cat - ; } | telnet -K 192.168.4.1 | tee tmp.res
+echo "Report"
+echo "======"
+cat tmp.res | egrep "(#.*|>.*)"
+#rm tmp.res
+networksetup -setairportpower $WIFI_INTERFACE off
+networksetup -getairportpower $WIFI_INTERFACE 
+
+
