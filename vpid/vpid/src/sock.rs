5fec492ed0d6e49f01962b408f33638f6343e9ba
L Diaz <ldiaz@navarone.local>
2020-09-23 15:20:56 +0200

    refactor socket server with common exec_command function + Json result as boolean

--- vpid/vpid/src/sock.rs
+++ vpid/vpid/src/sock.rs
@@ -1,22 +1,23 @@
-///! Socket control mdodule
-/// 
+///! Socket control module
+///! The main mechanims to
 
 use crate::error::{Result,ResultExt,SockBind};
 use std::thread;
 use std::thread::JoinHandle;
 use std::os::unix::net::{UnixListener};
-use crossbeam_channel::{bounded,Sender};
+use crossbeam_channel::{Sender,bounded};
 use std::path::PathBuf;
-use crate::{VpiCommand,VpiCommandBody};
+use crate::cmd::{VpiCommand,exec_command};
 use std::io::{Write,BufRead,BufReader};
 use std::net::Shutdown;
-use std::time::Duration;
-use vpi::cmd::VpiCmd;
+
+
 
 pub fn run_socket(sock:&PathBuf,command_sender_orig: &Sender<VpiCommand>) -> Result<JoinHandle<()>> {
     let listener = UnixListener::bind(sock).context( SockBind { sock: sock } )?;
-    let (bc_sender,bc_receiver) = bounded::<String>(1);
-    let command_sender= command_sender_orig.clone();
+    let (bc_sender,bc_receiver) = bounded::<String>(1); // Back channel for the response
+    let command_sender= command_sender_orig.clone(); // Clone the sender to move it to sock thread
+    // spawn thread for socket server
     let handle = thread::spawn( move | | {
         loop {
             match listener.accept() {
@@ -25,22 +26,16 @@ pub fn run_socket(sock:&PathBuf,command_sender_orig: &Sender<VpiCommand>) -> Res
                     let mut request = String::new();
                     match reader.read_line(&mut request) {
                         Ok(_len) => {
-                            let request_trimmed=request.trim();
+                            let request_trimmed=request.trim().to_string();
                             if request_trimmed.len() >0 {
-                                if let Some(cmd) = VpiCmd::from_string(request_trimmed.to_string()) {
-                                    if command_sender.send(VpiCommand::new(VpiCommandBody::Basic(cmd),Some(bc_sender.clone()))).is_ok() {
-                                        match bc_receiver.recv_timeout(Duration::from_secs(2)) {
-                                            Ok(val) => { let _=socket.write(val.as_bytes()); },
-                                            Err(_) =>  { let _=socket.write(r#"{"result":"error","data":"Internal error - command failed or timeout"}"#.as_bytes());}
-                                        };
-                                    } else {
-                                        let _=socket.write(r#"{"result":"error","data":"Internal error - can't deliver command"}"#.as_bytes());
-                                    }
-                                } else {
-                                    let _=socket.write(r#"{"result":"error","data":"Invalid command"}"#.as_bytes());
+                                // Process the request
+                                match exec_command(&request_trimmed, &command_sender, &bc_sender, &bc_receiver) {
+                                    Ok(val) => { let _=socket.write(val.as_bytes()); },
+                                    Err(e)  => { let _=socket.write(format!(r#"{{"result":false,"data":"{}"}}"#,e).as_bytes()); }
                                 }
+
                             } else {
-                                let _=socket.write(r#"{"result":"error","data":"command len 0"}"#.as_bytes());
+                                let _=socket.write(r#"{"result":false,"data":"command len 0"}"#.as_bytes());
                             }
                         },
                         Err(e) => error!("Socket server read failed:{}",e)
